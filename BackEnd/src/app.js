8a7a00d34a419ca8b26ddf0afa3758111c0a4fde
Kanishk19092003 <kanishkyadav141@gmail.com>
2026-02-08 12:43:53 +0530

    Implemented ai response structure in services

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -3,6 +3,10 @@ const aiRoutes = require("./routes/ai.routes");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
 app.get('/',(req,res)=>{
     res.send('Hello bhai');
 })
